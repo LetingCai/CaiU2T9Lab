3073e7a1e6303aa5ac5d2d737c0eeef634b5cb90
BT_3W2_04 <alvinl70@nycstudents.net>
2022-10-17 12:29:05 -0400

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 100; i++) {
+            int randomNum = (int)(Math.random()*36)+50;
+                    System.out.println(randomNum);
+        }
+
+    }
 }
--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class RightTriangle {
+public class RightTriangle {
+    double base;
+    double height;
+
+    public RightTriangle(double x, double y)
+    {
+        base = x;
+        height = y;
+    }
+
+    public double Hypotenuse()
+    {
+        return Math.sqrt(Math.pow(base,2)+Math.pow(height,2));
+    }
 }
